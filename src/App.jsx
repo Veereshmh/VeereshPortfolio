7b7375a8007e8ebf9c7e0e347ced972f52a05691
veeresh <veereshmh407@gmail.com>
2025-08-14 18:43:04 +0530

    added nav

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import { BrowserRouter, Routes } from "react-router-dom";
 import Languages from './components/languages/Languages'
 import Contact from './components/contact/Contact'
 import Notfound from './components/notfound/Notfound'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
 
@@ -22,6 +23,10 @@ function App() {
     <BrowserRouter basename="/VeereshPortfolio">
     <div className="w-[100%] mx-auto">
       <Router>
+        <nav className="flex gap-6 mb-6">
+          <Link to="/" className="text-blue-500 hover:underline">Home</Link>
+          <Link to="/contact" className="text-blue-500 hover:underline">Contact</Link>
+        </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
