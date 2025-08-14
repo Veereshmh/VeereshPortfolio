1e8bb8e5f13b34e6885ba7da3466b325274ca418
veeresh <veereshmh407@gmail.com>
2025-08-14 18:52:51 +0530

    contact page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Footer() {
+  const navigate = useNavigate();
   return (
     <footer className="bg-black text-white py-4">
       <div className="max-w-6xl mx-auto px-4 flex flex-col sm:flex-row items-center">
         <p className="text-sm pr-2">Veeresh Hiremath</p>
         <span className="hidden sm:inline">|</span>
         <a
-          href="#contact"
+          href="" onClick={() => navigate("/contact")}
           className="text-sm hover:text-gray-300 transition-colors pl-2"
         >
           Contact Me
