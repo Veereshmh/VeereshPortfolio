f77ba123c1ef80b5ad5fdc16d712bfdcc2d8ec5e
veeresh <veereshmh407@gmail.com>
2025-10-03 18:42:59 +0530

    changes done app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Languages from './components/languages/Languages'
 import Contact from './components/contact/Contact'
 import Notfound from './components/notfound/Notfound'
 import ContactsTable from "./components/contact/ContactsTable";
-import ThankYou from ".components/contact//ThankYou";
+import ThankYou from "./components/contact//ThankYou";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 // Temporary Home component
